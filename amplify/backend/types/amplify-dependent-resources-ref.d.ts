7edadbc4790fa178cbc186197d301bafe8803259
spartanDavid22 <90160219+spartanDavid22@users.noreply.github.com>
2022-09-04 21:48:47 -0400

    added amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "warnerbrosdash39eb2f6f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
